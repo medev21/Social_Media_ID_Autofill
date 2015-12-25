e5af5d90bcaab6e67bcee81bc2c02175a8644d04
Martin Ben <mebenav88@gmail.com>
2015-12-24 20:17:18 -0600

    add automatic url link after social media input is onchnage

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-str= document.getElementById("linkedinLink").value;
+function changeLinkedin(val){
 
-console.log('hello world');
+  var linkedin = document.getElementById("linkedinLink").value;
+  var linkedinWebsite = 'https://www.linkedin.com/in/';
+
+  document.getElementById('linkedinLink').value = linkedinWebsite + linkedin;
+
+  console.log(linkedin);
+}
+
+function changeGithub(val){
+
+  var github = document.getElementById("githubLink").value;
+  var githubWebsite = 'https://github.com/';
+
+  document.getElementById('githubLink').value = githubWebsite + github;
+
+  console.log(github);
+}

c5fbc23b1b58933ec383f96dfbd2d17e9593f740
Martin Ben <mebenav88@gmail.com>
2015-12-29 01:56:05 -0600

    add conditional statement to ignore automatic url add on if user provided the whole url link

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 function changeLinkedin(val){
 
-  var linkedin = document.getElementById("linkedinLink").value;
-  var linkedinWebsite = 'https://www.linkedin.com/in/';
+  var linkedin = document.getElementById("linkedinLink").value; //get input val
+  var linkedinWebsite = 'https://www.linkedin.com/in/'; //linkedin home url
+  var conditionOne = new RegExp('linkedin');
+  var conditionTwo = new RegExp('https');
+  var conditionThree = new RegExp('www');
 
-  document.getElementById('linkedinLink').value = linkedinWebsite + linkedin;
+  if( conditionOne.test(linkedin) || conditionTwo.test(linkedin) || conditionThree.test(linkedin) ){
+    console.log('with condition');
+    document.getElementById('linkedinLink').value = linkedin;
 
-  console.log(linkedin);
+  }else{
+    console.log('just the user id');
+    return document.getElementById('linkedinLink').value = linkedinWebsite + linkedin;
+  }
 }
 
 function changeGithub(val){
 
-  var github = document.getElementById("githubLink").value;
-  var githubWebsite = 'https://github.com/';
+  var github = document.getElementById("githubLink").value; //get input val
+  var githubWebsite = 'https://github.com/';  //github home url
+  var conditionOne = new RegExp('github');
+  var conditionTwo = new RegExp('https');
+  var conditionThree = new RegExp('www');
 
-  document.getElementById('githubLink').value = githubWebsite + github;
-
-  console.log(github);
+  //test if var github contains github home url
+  if( conditionOne.test(github) || conditionTwo.test(github) || conditionThree.test(github) ){
+    console.log('w/ condition');
+    document.getElementById('githubLink').value = github;
+  }else{
+    document.getElementById('githubLink').value = githubWebsite + github;
+    console.log('w/ userid');
+  }//end of if/else cond.
 }
